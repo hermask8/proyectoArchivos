c13572f1bcd17a149b3409967eda22e286fc6761
hermask8 <hermask8@gmail.com>
2018-09-29 14:05:55 -0600

    Agregar escritura

--- src/proyecto1/MetodosBitacora.java
+++ src/proyecto1/MetodosBitacora.java
@@ -5,10 +5,102 @@
  */
 package proyecto1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.DefaultListModel;
+
 /**
  *
  * @author ervi
  */
 public class MetodosBitacora {
+    
+    public boolean LlenarArchivo(String strPath,String strContenido,String strError)
+    {
+        File Archivo = new File(strPath);
+        
+        try
+        {
+                FileWriter Escribir = new FileWriter(Archivo,true);
+                BufferedWriter bw = new BufferedWriter(Escribir);
+                bw.write(strContenido + System.lineSeparator());
+                bw.close();
+                Escribir.close();
+                
+                return true;
+        }
+        catch(IOException ex)
+        {
+            strError= ex.getMessage();
+            return false;
+        } 
+        
+    }
+    
+    public boolean Obtener(String strPath,String strError)
+    {
+        File Archivo = new File(strPath);
+        if(!Archivo.exists())
+        {
+            strError="No existe el archivo";
+            return false;
+        }
+        else
+        {
+            FileReader LecturaArchivo;
+            try {
+                LecturaArchivo = new FileReader(Archivo);
+                BufferedReader LeerArchivo = new BufferedReader(LecturaArchivo);
+                String Linea="";
+                try {
+                    Linea=LeerArchivo.readLine();
+                    String[] split;
+                    DefaultListModel modeloNombre= new DefaultListModel();
+                    DefaultListModel modeloApellido= new DefaultListModel(); 
+                    while(Linea != null)
+                    {
+                        if(!"".equals(Linea))
+                        {
+                            split=Linea.split(",");
+                            modeloNombre.addElement(split[0]);
+                            modeloApellido.addElement(split[1]);
+                        }
+                        Linea=LeerArchivo.readLine();
+                    }
+
+                    LecturaArchivo.close();
+                    LeerArchivo.close();
+                    
+                    //lstNombres.setModel(modeloNombre);
+                    //lstApellidos.setModel(modeloApellido);
+                    strError="";
+                    return true;
+                    
+                } catch (IOException ex) {
+                    strError= ex.getMessage();
+                    return false;
+                }
+            } catch (FileNotFoundException ex) {
+                strError= ex.getMessage();
+                return false;
+            }
+        }
+    }
+    
+    public boolean ValidarLLeno()
+    {
+        return false;
+    }
+    
+    public boolean EscribirDescVitacora()
+    {
+        return false;
+    }
+    
     
 }
--- src/proyecto1/MetodosUsuarios.java
+++ src/proyecto1/MetodosUsuarios.java
@@ -5,10 +5,35 @@
  */
 package proyecto1;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author ervi
  */
 public class MetodosUsuarios {
-    
+    public boolean LlenarArchivo(String strPath,String strContenido,String strError)
+    {
+        File Archivo = new File(strPath);
+        
+        try
+        {
+                FileWriter Escribir = new FileWriter(Archivo,true);
+                BufferedWriter bw = new BufferedWriter(Escribir);
+                bw.write(strContenido + System.lineSeparator());
+                bw.close();
+                Escribir.close();
+                
+                return true;
+        }
+        catch(IOException ex)
+        {
+            strError= ex.getMessage();
+            return false;
+        } 
+        
+    }
 }
